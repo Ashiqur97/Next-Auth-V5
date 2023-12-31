6e3cda9eecadaf833aabe8d8d6a050a8e0e0936d
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-12-31 18:38:04 +0600

    initial code

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,16 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>I am page</div>
+    <main className='flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
+      <div className='space-y-6 text-center'>
+          <h1 className='ext-6xl font-semibold text-white drop-shadow-md'>
+              🔐 Auth
+          </h1>
+
+          <p className="text-white text-lg">
+          A simple authentication service
+        </p>
+      </div>
+    </main>
   )
 }
