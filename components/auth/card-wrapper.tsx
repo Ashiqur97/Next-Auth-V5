57f35ea006bc2c9447ddae779454e7244da158b4
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-01-01 19:07:55 +0600

    initial commit change

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -1,54 +1,49 @@
-"use client"
+"use client";
 
 import { 
-    Card,
-    CardContent,
-    CardFooter,
-    CardHeader
-  } from "@/components/ui/card";
-
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader
+} from "@/components/ui/card";
 import { Header } from "@/components/auth/header";
 import { Social } from "@/components/auth/social";
+import { BackButton } from "@/components/auth/back-button";
 
 interface CardWrapperProps {
-    children: React.ReactNode;
-    headerLabel: string;
-    backButtonLabel: string;
-    backButtonHref: string;
-    showSocial?: boolean;
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
 };
 
 export const CardWrapper = ({
-    children,
-    headerLabel,
-    backButtonLabel,
-    backButtonHref,
-    showSocial
-
+  children,
+  headerLabel,
+  backButtonLabel,
+  backButtonHref,
+  showSocial
 }: CardWrapperProps) => {
-    return (
-        <Card className="w-[400px] shadow-md">
-        <CardHeader>
-          <Header label={headerLabel} />
-        </CardHeader>
-        <CardContent>
-          {children}
-        </CardContent>
-        {showSocial && (
-            <CardFooter>
-                <Social />
-            </CardFooter>
-        )}
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel} />
+      </CardHeader>
+      <CardContent>
+        {children}
+      </CardContent>
+      {showSocial && (
         <CardFooter>
-            <BackButton>
-            label={backButtonLabel}
-            href={backButtonHref}
-            </BackButton>
+          <Social />
         </CardFooter>
-      </Card>
-    );
+      )}
+      <CardFooter>
+        <BackButton
+          label={backButtonLabel}
+          href={backButtonHref}
+        />
+      </CardFooter>
+    </Card>
+  );
 };
-
-
-
-
