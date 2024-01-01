f71452d72d86846dd20a1f1a02479621d0bfb9e7
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-01-01 20:08:25 +0600

    second commit

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,7 +1,31 @@
+"use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { useState, useTransition } from "react";
+import { useSearchParams } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { Input } from "@/components/ui/input";
 import {CardWrapper} from "@/components/auth/card-wrapper";
+import { LoginSchema } from "@/schemas";
 
-
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,  
+} from "@/components/ui/form";
 const LoginForm = () => {
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+          email: "",
+          password: "",
+        },
+      });
     return ( 
         <CardWrapper
             headerLabel="Welcome Back!"
