23ddef54f4b171ee04690b2845e4a134e6f0df58
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-01-01 17:12:09 +0600

    code commit

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,9 +1,6 @@
-const AuthLayout = ({children}: any) => {
+const AuthLayout = ({children} : {children: React.ReactNode}) => {
     return ( 
-        <div>
-            <nav className="bg-red-500 text-white">
-                This is auth navbar 
-            </nav>
+        <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
             {children}
         </div>
      );
