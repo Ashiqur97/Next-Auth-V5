38a0afb3fc6a9269d7be3d20e079e6f029c1f14f
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-01-10 18:43:54 +0600

    initial code

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,12 @@
 import { auth } from "./auth"
 export default auth((req) => {
+    const isLoggedIn = !!req.auth;
   console.log("ROUTE:", req.nextUrl.pathname)
+  console.log("IS_LOGGED_IN:", isLoggedIn);
 })
 // Optionally, don't invoke Middleware on some paths
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
-    matcher: ["/auth/login", "/auth/register"],
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
   }
 
