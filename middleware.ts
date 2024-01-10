42fd21d15692443a84f4eaf22468c4f1a0b6e0cd
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-01-10 23:52:51 +0600

    code commit

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import authConfig from "./auth.config";
+import authConfig from "@/auth.config";
 import { DEFAULT_LOGIN_REDIRECT,apiAuthPrefix,authRoutes,publicRoutes } from "@/routes";
 
 const {auth} = NextAuth(authConfig);
@@ -13,8 +13,20 @@ export default auth((req) => {
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
   if(isApiAuthRoute) {
-    return true;
+    return null;
   }
+
+  if(isAuthRoute){
+    if(isLoggedIn){
+      return Response.redirect(DEFAULT_LOGIN_REDIRECT,nextUrl);
+    }
+    return null;
+  }
+
+  if(!isLoggedIn && !isPublicRoute){
+    return Response.redirect(new URL("/auth/login",nextUrl));
+  }
+  return null;
 })
 // Optionally, don't invoke Middleware on some paths
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
