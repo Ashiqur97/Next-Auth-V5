3acd635b00c8be384c64b4de060c80163c28fa0b
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-01-13 16:44:41 +0600

    code commit

--- auth.ts
+++ auth.ts
@@ -1,7 +1,8 @@
-import NextAuth from "next-auth"
+import NextAuth,{DefaultSession} from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "@/auth.config";
 import {db} from "@/lib/db";
+import { getUserById } from "@/data/user";
 
 export const {
   handlers: { GET, POST },
@@ -14,9 +15,22 @@ export const {
         if(token.sub && session.user){
           session.user.id = token.sub;
         }
-        return session
+
+        if(token.role && session.user) {
+          session.user.role = token.role;
+        }
+
+        return session;
     },
-    async jwt({token}) {
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
       return token
     }
   },
