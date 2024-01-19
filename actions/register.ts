4dedfd102851f0e0c44c8517bb862868996ea7c0
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2024-01-20 00:36:18 +0600

    code change

--- actions/register.ts
+++ actions/register.ts
@@ -1,44 +1,43 @@
-"use server"
+"use server";
 
-import bcrypt from "bcrypt";
-import { RegisterSchema} from "@/schemas"
-import * as z from "zod"
-import{db} from "@/lib/db"
+import * as z from "zod";
+import bcrypt from "bcryptjs";
+
+import { db } from "@/lib/db";
+import { RegisterSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
+import { sendVerificationEmail } from "@/lib/mail";
 import { generateVerificationToken } from "@/lib/tokens";
 
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values);
 
-export const register = async(values: z.infer<typeof RegisterSchema>) => {
-    const validatedFields = RegisterSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
 
-    if(!validatedFields.success) {
-        return {error: "Invalid fields!"};
-    }
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const {email,password,name} = validatedFields.data;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const existingUser = await getUserByEmail(email);
+  const existingUser = await getUserByEmail(email);
 
-    if (existingUser) {
-        return { error: "Email already in use!" };
-      }
-    
-      await db.user.create({
-        data: {
-          name,
-          email,
-          password: hashedPassword,
-        },
-      });
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
 
-      const verificationToken = await generateVerificationToken(email);
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
 
+  const verificationToken = await generateVerificationToken(email);
+  await sendVerificationEmail(
+    verificationToken.email,
+    verificationToken.token,
+  );
 
-    return {success: "Confirmation email sent!"};
+  return { success: "Confirmation email sent!" };
 };
-
-
-
-
-
-
